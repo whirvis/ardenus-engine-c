b32b99c058b1c865a60fc0e706d909079bb74a52
Trent Summerlin <whirvis@ardenus.org>
2021-08-02 16:38:25 -0500

    Create VorbisFile.h

---
+++ include/VorbisFile.h
@@ -0,0 +1,20 @@
+#ifndef VORBIS_FILE_H_
+#define VORBIS_FILE_H_
+
+#ifndef STB_VORBIS_HEADER_ONLY
+	#define STB_VORBIS_HEADER_ONLY
+	#include "stb_vorbis.c"
+	#undef STB_VORBIS_HEADER_ONLY
+#endif
+
+typedef struct VorbisFile VorbisFile;
+
+VorbisFile * VorbisFile_open(const char *path);
+
+int VorbisFile_getALFormat(const VorbisFile *info);
+int VorbisFile_getFrequencyHz(const VorbisFile *info);
+int VorbisFile_getChannelCount(const VorbisFile *info);
+int VorbisFile_getBitsPerSample(const VorbisFile *info);
+int VorbisFile_readPCM(VorbisFile *info, int offset, char *buf, int len);
+
+#endif
